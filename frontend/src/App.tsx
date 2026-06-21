dec96c7b772229f33e8365a92c0a27e08479f1e5
Kotbuz <wasdtv03@gmail.com>
2026-06-21 21:55:25 +0700

    Добавил файлы предметов и рецептов из последней версии майнкрафта для дальнейшей работы. Добавил базовую структуру фронтенда, добавил возможность локального запуска фронтенда, добавил node modules в игнор

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './styles/App.css';
+
+function App() {
+    return (
+        <div className="app">
+            <header className="app-header">
+                <h1>Recipe Tree Visualizer</h1>
+                <p>React + TypeScript frontend structure is ready.</p>
+            </header>
+        </div>
+    );
+}
+
+export default App;
